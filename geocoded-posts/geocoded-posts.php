ebf975e1471baccd6be835e26eb1c23ffd8afc5b
Stephan van Rooij <stephan.vanrooij@fontys.nl>
2017-05-14 13:31:07 +0200

    Adding settings link

--- geocoded-posts/geocoded-posts.php
+++ geocoded-posts/geocoded-posts.php
@@ -10,7 +10,7 @@
  * Plugin Name:         Geocoded posts
  * Plugin URI:          https://github.com/svrooij/wp-geocoded-posts
  * Description:         Widget with geocoded posts and editing geo location on a post.
- * Version:             0.0.3
+ * Version:             0.0.4
  * Author:              Stephan van Rooij
  * Author URI:          https://svrooij.nl
  * License:             MIT
@@ -25,7 +25,7 @@
     * Constant with the version number.
     *
     */
-  const VERSION = '0.0.3';
+  const VERSION = '0.0.4';
 
    /**
 	 * Static property to hold our singleton instance
@@ -45,19 +45,22 @@
     require_once('includes/class-geocoded-posts-widget.php');
     add_action('widgets_init', array( $this, 'register_widgets'));
 
-
 		if(is_admin()){
       require_once('includes/class-geocoded-posts-editor.php');
       require_once('includes/class-geocoded-posts-settings.php');
+
+      // Adding a settings link to the plugin menu.
+      $plugin = plugin_basename(__FILE__);
+      add_filter("plugin_action_links_$plugin", array ($this , 'settings_link'));
     }
 
-    // Can this be put in is_admin? OR should I just leave it here?
-    if(boolval(get_option('geocoded_posts_auto_geocode'))){
-      require_once('includes/class-geocoded-posts-geocoder.php');
-    }
+    // // Can this be put in is_admin? OR should I just leave it here?
+    require_once('includes/class-geocoded-posts-geocoder.php');
 
-    // Include REST Api extension if the api is loaded.
-    add_action ('plugins_loaded', array($this, 'rest_extension'), 11);
+    // Include REST Api extension if the API exists. (in core since 4.4)
+    if(class_exists( 'WP_REST_Controller' )){
+      require_once('includes/class-geocoded-posts-rest-api.php');
+    }
 	}
 
   /**
@@ -85,12 +88,12 @@
     register_widget( 'Geocoded_Posts_Widget' );
   }
 
-  public function rest_extension(){
-    if(class_exists( 'WP_REST_Controller' )){
-      require_once('includes/class-geocoded-posts-rest-api.php');
-    }
+  // Add settings link on plugin page
+  public function settings_link($links) {
+    $settings_link = '<a href="options-writing.php">'.__('Settings','geocoded-posts').'</a>';
+    array_unshift($links, $settings_link);
+    return $links;
   }
-
 
  }
 
